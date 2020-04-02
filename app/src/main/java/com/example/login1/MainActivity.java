fdb40885509be188ef4d50a1e96399c77d264ecd
Dinh Quang <quangddinh@gmail.com>
2020-04-02 13:01:07 +0700

    Login

---
+++ app/src/main/java/com/example/login1/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.login1;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtTk, edtMk ;
+    Button btnDangnhap;
+    Button mBtnRegister;
+    private FirebaseAuth mAuth;
+    int mRequestCode = 111;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnRegister = findViewById(R.id.btn_register);
+
+
+        map();
+        setListener();
+
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private void setListener() {
+
+        btnDangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email2 = edtTk.getText().toString();
+                String pass2 = edtMk.getText().toString();
+
+                if ( email2.equals("") || pass2.equals(""))
+                {
+                    Toast.makeText(MainActivity.this , "Bạn Chưa Nhập Thông Tin", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else
+                {
+                mAuth.signInWithEmailAndPassword(email2, pass2)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(MainActivity.this, "Log in success", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(MainActivity.this, "Fail!!!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                }
+            }
+        });
+
+    }
+
+    private void map() {
+        edtTk = findViewById(R.id.edittextEmail);
+        edtMk = findViewById(R.id.edittextPassword);
+        btnDangnhap = findViewById(R.id.buttonDangnhap);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Toast.makeText(this, currentUser.getEmail(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == mRequestCode && resultCode == RESULT_OK && data != null) {
+
+            String email2 = data.getStringExtra(RegisterActivity.EMAIL);
+            String pass2 = data.getStringExtra(RegisterActivity.PASS);
+
+            Toast.makeText(MainActivity.this, "Insert success", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "No data", Toast.LENGTH_SHORT).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/example/login1/RegisterActivity.java
@@ -0,0 +1,105 @@
+package com.example.login1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Button mBtnSign, mBtnTrangchu, mBtnBack;
+    EditText mEdtemail, mEdtpass;
+    private FirebaseAuth mAuth;
+    public static final String EMAIL = "EMAIL";
+    public static final String PASS = "PASS";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        mBtnSign = findViewById(R.id.btn_signup);
+        mEdtemail = findViewById(R.id.edt_email);
+        mEdtpass = findViewById(R.id.edt_pass);
+        mBtnTrangchu = findViewById(R.id.btn_trangchu);
+        mBtnBack = findViewById(R.id.btn_back);
+
+
+        mBtnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEdtemail.getText().toString();
+                String password = mEdtpass.getText().toString();
+                if(email.equals("") || password.equals("") )
+                {
+                    Toast.makeText(RegisterActivity.this , "Bạn Chưa Nhập Thông Tin", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else
+                {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(RegisterActivity.this, "Sign up success", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                }
+            }
+        });
+
+        mBtnTrangchu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email1 = mEdtemail.getText().toString();
+                String pass1 = mEdtpass.getText().toString();
+
+                if(email1.equals("") || pass1.equals("") )
+                {
+                    Toast.makeText(RegisterActivity.this , "Bạn Chưa Nhập Thông Tin", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else {
+                    Intent intent = new Intent();
+                    intent.putExtra(EMAIL, email1);
+                    intent.putExtra(PASS, pass1);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                    Toast.makeText(RegisterActivity.this , "Đăng kí thành công", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        mBtnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
